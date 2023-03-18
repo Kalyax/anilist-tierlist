3e40102df306e78d22cc9735b1f33978911dba57
Kalyax <kalyax@hotmail.com>
2023-03-19 00:30:53 +0100

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
     <SearchBar @fetchUser="(username: string) => fetchUser(username)"/>
-    <TierList class="mx-10" :user="user"/>
+    <TierList class="mx-10"/>
 </template>
 
 <script setup lang="ts">
 import TierList from './components/TierList.vue';
 import SearchBar from './components/SearchBar.vue';
 import { userQuery, userIdQuery } from './misc/queries';
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import fetchData from './misc/fetchData';
 import { useUserStore } from './stores/userStore';
 
 const userStore = useUserStore()
 
-interface User {
-    id: number,
-    name: string,
-    avatar: {
-        medium: string
-    },
-    mediaListOptions: {
-        scoreFormat: string
-    }
-}
-
 const urlParams = new URLSearchParams(window.location.search);
 /** user has to be initialised with url params in case there's an id and fetch the user if there's an id */
-const user = ref(<User>{
-    id: Number(urlParams.get("id"))
-})
+userStore.info.id = Number(urlParams.get("id"))
 /** if there is an id param, fetch the user when mounted */
 onMounted(async () => {
-    if(user.value.id != 0) fetchUser(user.value.id)
+    if(userStore.info.id && userStore.info.id != 0) fetchUser(userStore.info.id)
 })
 
 /**
@@ -45,10 +32,7 @@ async function fetchUser(userIdentifier: string | number){
     fetchData(query, variables)
         .then((res) => {
             if(res.data.User === null) console.error("No user found with this name")
-            else {
-                user.value = <User> res.data.User
-                userStore.info = res.data.User
-            }
+            else userStore.info = res.data.User
         })
         .catch((err) => {
             throw err;
