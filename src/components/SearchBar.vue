61236cd46c2a64ae18c290c4698b5205a61cfa7d
Kalyax <kalyax@hotmail.com>
2023-03-28 16:51:47 +0200

    enhanced ui

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,19 +4,24 @@
             <img :src="userStore.info.avatar?.medium" width="48" height="48" class="rounded-full">
             <p class="text-xl font-bold mb-2 md:mb-0">{{ userStore.info.name }}</p>
         </a>
-        <div class="w-full text-center mb-2 md:mb-0">
+        <div class="w-full text-center mb-2 md:mb-0 flex items-center justify-center">
             <input v-model="username" type="text" 
                 class="w-2/5 bg-slate-800 hover:bg-slate-800/80 focus:bg-slate-800/50 transition-colors px-4 py-2 rounded-xl outline-none" 
                 placeholder="Type an username...">
             <button 
-                class="bg-sky-500 hover:bg-sky-600 focus:bg-green-500 transition-colors duration-300 font-bold rounded-xl ml-3 px-3 py-2" 
+                class="hover:bg-slate-800 focus:bg-green-500 transition-colors duration-300 font-bold rounded-xl ml-3 px-3 py-2" 
                 @click="copyLink">
-                Copy link
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                </svg>
             </button>
             <button 
-                class="bg-sky-500 hover:bg-sky-600 focus:bg-green-500 transition-colors duration-300 font-bold rounded-xl ml-3 px-3 py-2" 
+                class="hover:bg-slate-800 transition-colors duration-300 font-bold rounded-xl ml-3 px-3 py-2" 
                 @click="$emit('openSettings')">
-                Settings
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
             </button>
             <input type="text" :value="getLink" id="link" class="hidden">
         </div>
--- src/components/SettingsPopup.vue
+++ src/components/SettingsPopup.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black/50">
-        <div class="bg-slate-900 w-1/2 h-1/2 rounded-xl px-10 py-8 overflow-y-scroll">
-            <h1 class="font-bold text-2xl">Settings</h1>
-            <button @click="emit('closeSettings')">close</button>
-            <button @click="sortMedia()">test</button>
-            <div class="bg-slate-800 space-x-3 px-2 py-2 w-fit rounded-xl">
-                <span disabled>10 Point Decimal</span>
-                <span disabled>100 Point</span>
-                <span disabled>10 Point</span>
-                <span disabled>5 Star</span>
-                <span disabled>3 Point Smiley</span>
+        <div class="bg-slate-900 min-w-1/2 min-h-2/3 rounded-xl px-10 py-8 overflow-y-scroll">
+            <div class="flex justify-between">
+                <h1 class="font-bold text-2xl">Settings</h1>
+                <button @click="emit('closeSettings')" class="hover:bg-slate-800 rounded-full transition-colors">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </div>
-            <div>
-                <h2 class="text-xl">Tiers <span class="font-light italic text-sm">From excluded, To included</span></h2>
+            <div class="text-center">
+                <h2 class="text-xl">Tiers</h2>
+                <p class="font-light italic text-sm  mb-2">From excluded, To included</p>
                 <div class="space-y-2">
-                    <button class="bg-red-800" @click="addTier()">Add tier</button>
+                    <button class="px-3 py-2 bg-cyan-600 hover:bg-cyan-700 transition-colors rounded-xl font-bold" @click="addTier()">Add tier</button>
                     <div v-for="tier, i in userStore.tiers" class="flex items-center space-x-2">
-                        <p :class="tier.color" class="text-slate-900 font-bold text-xl w-10 text-center py-3 rounded-xl mr-1">{{ tier.name }}</p>
+                        <p :class="tier.color" class="text-slate-900 font-bold text-xl w-10 text-center py-2 rounded-xl mr-1">{{ tier.name }}</p>
                         <input type="text" v-model="userStore.tiers[i].from" placeholder="From" class="w-20 bg-slate-800 hover:bg-slate-800/80 focus:bg-slate-800/50 transition-colors px-4 py-2 rounded-xl outline-none">
                         <input type="text" v-model="userStore.tiers[i].to" placeholder="To" class="w-20 bg-slate-800 hover:bg-slate-800/80 focus:bg-slate-800/50 transition-colors px-4 py-2 rounded-xl outline-none">
-                        <button class="bg-red-800" @click="removeTier(i)">Remove tier</button>
+                        <button class="px-3 py-1.5 bg-red-600 hover:bg-red-700 transition-colors rounded-xl font-bold" @click="removeTier(i)">Remove tier</button>
                     </div>
                 </div>
             </div>
@@ -30,13 +29,13 @@
 <script setup lang="ts">
 import { useUserStore } from './../stores/userStore';
 import type { Tier } from './../misc/types';
-import { sortMedia } from '@/misc/tiers/tierSorter';
 
 const userStore = useUserStore();
 
 const emit = defineEmits(["closeSettings"])
 
 function addTier(){
+    userStore.sortedTiers.push([])
     userStore.tiers.push({
         name: "X",
         color: "bg-slate-200",
